b21023ccf6495db53d229bcc51081e46c3a649b1
Aravind276 <maravindr904@gmail.com>
2026-03-12 14:36:42 +0530

    add root endpoint

--- src/main/java/com/lostandfound/DemoApplication.java
+++ src/main/java/com/lostandfound/DemoApplication.java
@@ -2,16 +2,23 @@ package com.lostandfound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		
 		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String home() {
+		return "Backend deployed and running successfully!";
+	}
 }
